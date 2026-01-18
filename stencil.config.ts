bf1f35b66e1c2cce08c72182783f9f0e158323d8
hyunmin <badagogi@gmail.com>
2026-01-18 22:15:00 +0900

    chore: 1차 배포 버전

--- stencil.config.ts
+++ stencil.config.ts
@@ -1,8 +1,14 @@
 import { Config } from '@stencil/core';
+import { reactOutputTarget } from '@stencil/react-output-target';
 
 export const config: Config = {
   namespace: 'my-health-report',
   outputTargets: [
+    reactOutputTarget({
+      outDir: '../my-health-report-react/src/stencil-generated/',
+      // Next.js App Router SSR을 고려하면 hydrateModule 설정을 검토
+      // hydrateModule: 'dist/hydrate/index.js' // 프로젝트 설정에 맞게
+    }),
     {
       type: 'dist',
       esmLoaderPath: '../loader',
@@ -12,6 +18,7 @@ export const config: Config = {
       customElementsExportBehavior: 'auto-define-custom-elements',
       externalRuntime: false,
     },
+    
     {
       type: 'docs-readme',
     },
